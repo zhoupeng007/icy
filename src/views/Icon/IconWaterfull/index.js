23f8385e76ae9bacc2b275a12e00d70802166998
Your Name <you@example.com>
2019-06-27 08:18:13 +0800

    icon基本完成

--- src/views/Icon/IconWaterfull/index.js
+++ src/views/Icon/IconWaterfull/index.js
@@ -22,9 +22,9 @@ class IconWaterfull extends PureComponent {
                             this.setState({ refreshing: true })
                             axios.get(`/icym.php?method=icy.getCollocationList&appId=4&page=${this.state.page}&pageSize=10&categoryId=${this.state.id}`).then(res => {
                                 this.setState({
-                                    waterfulllist: [...this.state.waterfulllist,...res.data.data.list],
+                                    waterfulllist: [...this.state.waterfulllist, ...res.data.data.list],
                                     refreshing: false,
-                                    page:this.state.page++
+                                    page: this.state.pages++
                                 })
                             })
                         }}
@@ -32,46 +32,25 @@ class IconWaterfull extends PureComponent {
                         <ul className={scss.IconWaterfull_ul}>
                             {
                                 this.state.waterfulllist.map((item, index) =>
-                                    // index % 2 === 0
-                                         <li key={index} className={index === 1 ? scss.IconWaterfull_tsli : scss.IconWaterfull_li}>
-                                            <img src={this.imagesrc(item.image.image)} alt="暂无此图片" className={index === 1 ? scss.IconWaterfull_tsimg : scss.IconWaterfull_img} />
-                                            {
-                                                item.type === 0?
-                                                <div className={scss.IconWaterfull_bottom}>
-                                                    <img src={this.imagesrc(item.avatar)} alt="暂无此图片" className={scss.IconWaterfull_bottom_img} />
-                                                    {/* <img src={this.imagesrc(item.identityIcon.image)} alt="暂无此图片" className={scss.IconWaterfull_bottom_smallimg} /> */}
-                                                    <p className={scss.IconWaterfull_pu}>{item.username}</p>
-                                                    <p className={scss.IconWaterfull_pi}>{item.introduce}</p>
-                                                </div>
-                                            :''
-                                            }
-                                        </li>
-                                        // : ''
-                                )
-                            }
-                        </ul>
-                    {/* <ul className={scss.IconWaterfull_ul}>
-                        {
-                            this.state.waterfulllist.map((item, index) =>
-                                index % 2 !== 0
-                                    ? <li key={item.collocationId + "c"} className={index === 1 ? scss.IconWaterfull_tsli : scss.IconWaterfull_li}>
+                                    <li key={index} className={index === 1 ? scss.IconWaterfull_tsli : scss.IconWaterfull_li}>
                                         <img src={this.imagesrc(item.image.image)} alt="暂无此图片" className={index === 1 ? scss.IconWaterfull_tsimg : scss.IconWaterfull_img} />
                                         {
                                             item.type === 0 ?
                                                 <div className={scss.IconWaterfull_bottom}>
                                                     <img src={this.imagesrc(item.avatar)} alt="暂无此图片" className={scss.IconWaterfull_bottom_img} />
-                                                    {/* <img src={this.imagesrc(item.identityIcon.image)} alt="暂无此图片" className={scss.IconWaterfull_bottom_smallimg} /> */}
-                                                    {/* <p className={scss.IconWaterfull_pu}>{item.username}</p>
+                                                    <img src="https://image3.ichuanyi.cn/group3/M00/9E/46/CgAAGlhwadAEAAAAAAAAAM6Loeg797.png" alt="暂无此图片" className={scss.IconWaterfull_bottom_smallimg} />
+
+                                                    <p className={scss.IconWaterfull_pu}>{item.username}</p>
                                                     <p className={scss.IconWaterfull_pi}>{item.introduce}</p>
                                                 </div>
                                                 : ''
-
                                         }
                                     </li>
-                                    : ''
-                            )
-                        }
-                    </ul> */}
+                                )
+                            }
+
+                        </ul>
+
                     </PullToRefresh>
                 </div>
             </div>
@@ -85,16 +64,17 @@ class IconWaterfull extends PureComponent {
     state = {
         waterfulllist: [],
         id: 0,
-        refreshing:false,
-        height:0,
-        page:2
+        refreshing: false,
+        height: 0,
+        page: 2,
+        pages: 2,
     }
     componentDidMount() {
-        console.log(ReactDOM.findDOMNode(this.ptr).offsetTop)
+        // console.log(ReactDOM.findDOMNode(this.ptr).offsetTop)
         axios.get(`/icym.php?method=icy.getCollocationList&appId=4&page=1&pageSize=10&categoryId=${this.state.id}`).then(res => {
             this.setState({
                 waterfulllist: res.data.data.list,
-                height: document.documentElement.clientHeight-ReactDOM.findDOMNode(this.ptr).offsetTop+"px"
+                height: document.documentElement.clientHeight - ReactDOM.findDOMNode(this.ptr).offsetTop + "px"
             })
         })
     }
@@ -102,7 +82,7 @@ class IconWaterfull extends PureComponent {
         // console.log(props)
         this.setState({
             waterfulllist: props.waterfull,
-            id:props.myid
+            id: props.myid,
         })
     }
 }
