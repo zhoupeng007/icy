7dacfa21aec7f3d84ef3bbbbe0eb098d3faef53a
Your Name <you@example.com>
2019-06-27 11:26:18 +0800

    切换分支

---
+++ src/views/Designer/Xinpin/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+class Xinpin extends React.Component{
+  render(){
+    return (
+      <div>
+        <div style={{marginTop : '50px'}}>
+          <div></div>
+          <div>
+            <div>
+              <div>
+                <div>
+                  <p>新品入荷</p>
+                  <p></p>
+                </div>
+                <div>
+                  <p>查看更多</p>
+                </div>
+              </div>
+            </div>
+            <div></div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Xinpin
--- src/views/Designer/index.js
+++ src/views/Designer/index.js
@@ -3,6 +3,8 @@ import axios from 'axios'
 import Swipe from './swipe'
 import Shejishi from './Shejishi'
 import {connect} from 'react-redux'
+import Baokuan from './baokuan'
+import Xinpin from './Xinpin'
 class Designer extends React.Component{
   state = {
     list:[]
@@ -11,11 +13,12 @@ class Designer extends React.Component{
     return <div>
       <Swipe />
       <Shejishi list={this.state.list}/>
+      <Baokuan list={this.state.list}/>
+      <Xinpin list={this.state.list}/>
     </div>
   }
   componentWillMount(){
     axios.get('m.php?method=Userinfo.get&appId=4').then(res=>{
-      console.log(res)
     })
     axios.get('icym.php?method=icy.getHome&appId=4&page=1&pageSize=20').then(res=>{
       this.setState({
